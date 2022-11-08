e52aaeb570149f29ddb235c145bf1f5ffb7d6467
Tiago Costa <tiago.costa@pt.glory-global.com>
2022-11-08 19:28:23 +0000

    pyinstaller

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 # DNS Toggle for Windows
 
-This is a simple app that allows you to toggle between your default DNS and a custom DNS. 
+This is a simple app that allows you to toggle between your default DNS and a custom DNS from a tray icon.
 
 It is useful for when you want to use a Pi-Hole or other DNS server, but don't want to have to change your DNS settings every time you want to use it.
 
@@ -14,7 +14,13 @@ env\scripts\activate
 py -m pip install --upgrade pip
 pip install -r requirements.txt
 pip list --outdated
-py run.py "Network Bridge" "192.168.10.10"
+py run.py "Bridge de Rede" "192.168.50.89"
+
+pyinstaller --name DnsToggle --paths ".\env\Lib\site-packages\PyQt5\Qt\bin" --add-data='img/;img/' --clean --onefile --windowed --icon=app.ico run.py
+DnsToggle.exe "Bridge de Rede" "192.168.50.89"
+taskkill /im DnsToggle.exe /t /f
+
+netsh interface ipv4 show config name="Bridge de Rede"
 
 ```
 
--- run.py
+++ run.py
@@ -1,23 +1,37 @@
-import sys, re
+import sys, re, os
 from PyQt5 import QtGui, QtWidgets, QtCore
 
 adapter, dns_ip = sys.argv[-2:]
 app = QtWidgets.QApplication([])
 process = QtCore.QProcess()
 
+def resource_path(*path):
+    try:
+        base_path = sys._MEIPASS
+    except:
+        base_path = os.path.normpath(os.path.dirname(__file__))
+    return os.path.join(base_path, *path)
+
 def get_dns():
+    ''' Get the current DNS server for the given adapter '''
     process.start('netsh', ['interface', 'ipv4', 'show', 'config', f'name={adapter}'])
     process.waitForFinished()
     output = process.readAllStandardOutput().data().decode('utf-8')
     match = re.search(r"DNS.+?:\s+\b((?:[0-9]{1,3}\.){3}[0-9]{1,3})", output)
-    if match: return match.group(1)
-    return None
+    return match.group(1) if match else None
 
 dns_original = get_dns()
 widget = QtWidgets.QSystemTrayIcon()
 widget.setToolTip(f"DNS: {dns_original}")
 
+if dns_ip == dns_original: 
+    widget.setIcon(QtGui.QIcon(resource_path('img', 'on.svg')))
+    dns_original = None # we don't have an original DNS, so we go back to DHCP
+else:
+    widget.setIcon(QtGui.QIcon(resource_path('img', 'off.svg')))
+
 def toggle_dns():
+    ''' Toggle the active DNS server between the original and the given one '''
     current = get_dns()
     if current == dns_ip: 
         if dns_original is None:
@@ -26,17 +40,11 @@ def toggle_dns():
         else:
             process.start('netsh', ['interface', 'ipv4', 'set', 'dns', f'name={adapter}', 'static', dns_original ])
             widget.setToolTip(f"DNS: {dns_original}")
-        widget.setIcon(QtGui.QIcon('img/off.svg'))
+        widget.setIcon(QtGui.QIcon(resource_path('img', 'off.svg')))
     else:
         process.start('netsh', ['interface', 'ipv4', 'set', 'dns', f'name={adapter}', 'static', dns_ip ])
         widget.setToolTip(f"DNS: {dns_ip}")
-        widget.setIcon(QtGui.QIcon('img/on.svg'))
-
-if dns_ip == dns_original: 
-    widget.setIcon(QtGui.QIcon('img/on.svg'))
-    dns_original = None
-else:
-    widget.setIcon(QtGui.QIcon('img/off.svg'))
+        widget.setIcon(QtGui.QIcon(resource_path('img', 'on.svg')))
 
 widget.activated.connect(toggle_dns)
 widget.show()
